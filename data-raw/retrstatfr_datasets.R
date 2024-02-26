78ad7980e87bd4a904a6523c812b6813fc648561
patrickaubert <patrick.aubert@m4x.org>
2024-02-26 22:17:47 +0100

    Initie ajout de proj cor 2015 et 2016

--- data-raw/retrstatfr_datasets.R
+++ data-raw/retrstatfr_datasets.R
@@ -33,6 +33,29 @@ indiccor <- sources_opendata %>%
   filter(producteur=="COR",reference=="rapport annuel") %>%
   select(intitulecourt,datepubli)
 
+  # list des divers noms de scenarios (yc pb de format de lecture)
+list_noms_sc <- list(
+  "observé" = "obs",
+  "observée" = "obs",
+  "observations" = "obs",
+  "projetée" = "tous scénarios",
+  "1.7999999999999999e-2"="+1,8%/an",
+  "1.4999999999999999e-2"="+1,5%/an",
+  "1.2999999999999999e-2"="+1,3%/an",
+  "1.6e-2"="+1,6%/an",
+  "7.0000000000000001e-3"="+0,7%/an",
+  "0.02"="+2%/an",
+  "0.016"="+1,6%/an",
+  "0.013"="+1,3%/an",
+  "0.007"="+0,7%/an",
+  "0.01"="+1%/an",
+  "a'"="+2%/an",
+  "a"="+1,8%/an",
+  "b"="+1,5%/an",
+  "c"="+1,3%/an",
+  "c'"="+1%/an"
+)
+
   # fonction pour l'extraction des projections du cor
 extrait_projcor <- function(intitule,date){
   tab <- extrait_opendata(intitulecourt=intitule,datepubli=date) %>%
@@ -41,19 +64,7 @@ extrait_projcor <- function(intitule,date){
     rename(scenario=x1) %>%
     mutate(scenario = recode(
       scenario %>% tolower() %>% trimws(),
-      "observé" = "obs",
-      "observée" = "obs",
-      "observations" = "obs",
-      "projetée" = "tous scénarios",
-      "1.7999999999999999e-2"="+1,8%/an",
-      "1.4999999999999999e-2"="+1,5%/an",
-      "1.2999999999999999e-2"="+1,3%/an",
-      "1.6e-2"="+1,6%/an",
-      "7.0000000000000001e-3"="+0,7%/an",
-      "0.016"="+1,6%/an",
-      "0.013"="+1,3%/an",
-      "0.007"="+0,7%/an",
-      "0.01"="+1%/an"
+      !!! list_noms_sc
     ))
   names(tab) <- recode(names(tab),"valeurs"=intitule)
   return(tab)
@@ -64,6 +75,7 @@ extrait_projcor <- function(intitule,date){
 #names(tabindics) <- indiccor
 tabindics <- list()
 for (i in c(1:nrow(indiccor))){
+# for (i in c(46:53)){
   tabindics[[paste0(indiccor$intitulecourt[i],paste0(indiccor$datepubli[i]))]] <- extrait_projcor(indiccor$intitulecourt[i],paste0(indiccor$datepubli[i]))
 }
 
@@ -79,7 +91,13 @@ tablong <- do.call("bind_rows",tabindics) %>%
   select(-starts_with("x")) %>%
   # mise en forme longue
   pivot_longer(cols=-c("scenario","annee","datepubli"),names_to="serie",values_to="val") %>%
-  filter(!is.na(val))
+  filter(!is.na(val)) %>%
+  # nouvelle correction à la main
+  filter(!(grepl("^2015",datepubli) & grepl("convention cor",scenario))) %>%
+  mutate(scenario = case_when(
+    grepl("^2015",datepubli) ~ str_replace(scenario,"[[:space:]]*\\(convention.+$",""),
+    TRUE ~ scenario   ) %>%
+      recode(!!! list_noms_sc))
 
 # verif :
 # unique(tablong$scenario )
@@ -112,7 +130,7 @@ scenariosproj <- tablong %>% filter(!(scenario %in% c("obs","tous scénarios")))
   select(datepubli,scenario) %>% distinct() %>%
   rename(datepublisc=datepubli)
 
-sc_pour_dupli <- (scenariosproj %>% filter(datepublisc==min(datepubli)))$scenario
+sc_pour_dupli <- (scenariosproj %>% filter(datepublisc==min(tablong$datepubli)))$scenario
 
 tablong <- bind_rows(
   tablong %>% filter(scenario=="obs"),
